a505aacbb1efee4bc493a809868787af3e3841e0
Quyet Tran <zangquyet@gmail.com>
2017-12-17 23:00:55 +0700

    update

--- application/models/Nganhdaotao_model.php
+++ application/models/Nganhdaotao_model.php
@@ -15,13 +15,8 @@ class Nganhdaotao_model extends Cdio_Model {
 		$this->db->where('slug', $slug);
 		return $this->db->get($this->table_name)->row();
 	}
-	public function get_ctdt($id)
-	{
-	function getbySlug($slug) {
-		return $this->db->get_where($this->table_name, array('slug' => $slug))->row();
-	}
-	public function get_ctdt($id) {
 
+	public function get_ctdt($id) {
 		$this->db->join('nguoidung', 'nguoidung.id=nganh_dao_tao.last_id', 'left');
 		return $this->db->get_where($this->table_name, array($this->primary_key => $id))->row();
 	}
--- application/views/quyetdinh_chuandaura.php
+++ application/views/quyetdinh_chuandaura.php
@@ -1,28 +1,62 @@
-<div class="row">
+<div class="row" id="chuandaura">
 	<div class="col-md-12">
-		<h2>Quyết định chuẩn đầu ra</h2>
-		<div class="panel panel-default">
-			<div class="panel-heading">Nội dung</div>
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">Quyết định chuẩn đầu ra</h3>
+			</div>
 			<div class="panel-body">
-				<form action="" method="post" id="form">
-			<fieldset>
-				<div>
-					<textarea id="noidung" name="noidung">
-						<?=$qd->noidung?>
-					</textarea>
-
+				<div class="col-md-3">
+					<form @submit.prevent="uploadFile" action="" method="POST" role="form">
+						<legend>Upload chuẩn đầu ra</legend>
+					
+						<div class="form-group">
+							<label for="">Chọn file upload <small>(định dạng pdf)</small></label>
+							<input type="file" class="form-control" id="" placeholder="Input field">
+						</div>
+					
+					
+						<button type="submit" class="btn btn-primary">Upload</button>
+					</form>
 				</div>
-				<div style="margin-top: 10px; text-align: right;">
-					<input type="submit" class="btn btn-default" id="save" value="Save changes" /></div>
-			</fieldset>
-		</form>
+				<div class="col-md-3">
+					<div v-if="file!=null">
+						<object :data="this.base+file" type="application/pdf" width="100%" height="100%">
+						   <p><b>Example fallback content</b>: This browser does not support PDFs. Please download the PDF to view it: <a href="/pdf/sample-3pp.pdf">Download PDF</a>.</p>
+						</object>
+
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
+<script>
+	var vm = new Vue({
+		http: {
+		    emulateJSON: true,
+		    emulateHTTP: true
+		},
+		el: "#chuandaura",
+		data () {
+		  return {
+		    file:"",
+		    base:'<?php echo base_url() ?>'
+		  };
+		},
+		methods: {
+		  uploadFile () {
+		    alert();
+		  },
+		  getFile(){
+		  	this.$http.get(this.base+'Qd_chuandaura/api',data, options).then(res => {
+		  		this.file=res.body.file;
+		  	  console.log(res);
+		  	}).catch(err => {
+		  	  console.log(err);
+		  	});
+		  }
+		}
+	
+	})
+</script>
 
-<style>
-	iframe{
-    width: 100% !important;
-}
-</style>
